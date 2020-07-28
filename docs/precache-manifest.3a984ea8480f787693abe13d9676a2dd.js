9ee49e643bbe6977344ae3efb486a7080f29f109
Sonia-SV <sonia.sanzvivas@gmail.com>
2020-07-28 08:47:47 +0200

    First commit

---
+++ docs/precache-manifest.3a984ea8480f787693abe13d9676a2dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e32b04f92fc73386b431f126439f1b81",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ecbf972e833664b18ef",
+    "url": "./static/css/main.ecc87316.chunk.css"
+  },
+  {
+    "revision": "bf12df88b15339e3b67b",
+    "url": "./static/js/2.5bf8a30b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5bf8a30b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecbf972e833664b18ef",
+    "url": "./static/js/main.0ed04bb5.chunk.js"
+  },
+  {
+    "revision": "a0e6f4cee20c0c79b2e6",
+    "url": "./static/js/runtime-main.8bb1f59a.js"
+  }
+]);
